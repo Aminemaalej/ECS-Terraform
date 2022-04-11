911d73933fd4571aa4f20b079e87ed18ce73207f
Amine Maalej <amine.maalej@rewind.io>
2022-04-11 10:48:49 -0400

    add CW log group

--- ecs.tf
+++ ecs.tf
@@ -5,3 +5,12 @@ resource "aws_ecs_cluster" "aws-ecs-cluster" {
     Environment = var.app_environment
   }
 }
+
+resource "aws_cloudwatch_log_group" "log-group" {
+  name = "${var.app_name}-${var.app_environment}-logs"
+
+  tags = {
+    Application = var.app_name
+    Environment = var.app_environment
+  }
+}
